771a3b70a0679ae5ba092e5c7b6f3454584fb2db
Sukhorukov, Iurii <iurii.sukhorukov@ukg.com>
2020-10-27 18:52:31 +0300

    add note, change hashmap to treemap to keep dates order

--- src/main/java/com/epam/notekeeper/notekeeper/repository/NotesRepository.java
+++ src/main/java/com/epam/notekeeper/notekeeper/repository/NotesRepository.java
@@ -4,8 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 @Repository
 public class NotesRepository {
@@ -15,7 +15,7 @@ public class NotesRepository {
 
     public Map<String, String> getNotes() {
         return jdbcTemplate.query("SELECT date_of_creation, note FROM notes", resultSet -> {
-            Map<String, String> mapResult = new HashMap<>();
+            TreeMap<String, String> mapResult = new TreeMap<>();
             while (resultSet.next()) {
                 mapResult.put(resultSet.getString("date_of_creation"), resultSet.getString("note"));
             }
